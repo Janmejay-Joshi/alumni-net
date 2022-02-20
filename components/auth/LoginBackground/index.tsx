23090bb4aee0b35b0dc1773de8f6a55298d72ca4
Janmejay-Joshi <publish.janmejay@gmail.com>
2022-02-21 02:32:42 +0530

    Added Login Page

---
+++ components/auth/LoginBackground/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./LoginBackground.module.scss"
+
+
+export default function LoginBackground(){
+   
+  return (
+      <div className={styles.container}>
+        <img src="/assets/images/auth/BlobTL.svg" className={styles.TL} />
+        <img src="/assets/images/auth/BlobTR.svg" className={styles.TR}  />
+        <img src="/assets/images/auth/BlobBL.svg" className={styles.BL} /> 
+        <img src="/assets/images/auth/BlobBR.svg" className={styles.BR} />
+      </div>
+  )
+} 
---
+++ pages/auth/login.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import {
+  AppleLoginButton,
+  FacebookLoginButton,
+  GoogleLoginButton,
+} from "react-social-login-buttons";
+import LoginBackground from "../../components/auth/LoginBackground";
+import styles from "../../styles/PageStyles/Login.module.scss";
+
+interface LoginInput {
+  email: string;
+  password: string;
+}
+
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<LoginInput>();
+  const emailVerifyRegex =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  const onSubmit: SubmitHandler<LoginInput> = (data) => console.log(data);
+
+  return (
+    <>
+      <LoginBackground />
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <h2 className={styles.title}>{"Log In"}</h2>
+          <div className={styles.auth}>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              id="UserPass Form"
+              className={styles.userpass}
+            >
+              <div className={styles.input_container}>
+                <span>{"Email"}</span>
+                <input
+                  type={"email"}
+                  className={styles.input}
+                  {...register("email", {
+                    required: true,
+                    pattern: emailVerifyRegex,
+                  })}
+                />
+                {errors?.email?.type === "required" && (
+                  <p>{"This field is required"}</p>
+                )}
+                {errors?.email?.type === "pattern" && <p>{"Email only"}</p>}
+              </div>
+              <div className={styles.input_container}>
+                <span>{"Password"}</span>
+                <input
+                  type={"password"}
+                  className={styles.input}
+                  {...register("password", { required: true, minLength: 8 })}
+                />
+                {errors?.password?.type === "required" && (
+                  <p>{"This field is required"}</p>
+                )}
+                {errors?.password?.type === "minLength" && (
+                  <p>{"Password Should be Over 8 Characters"}</p>
+                )}
+              </div>
+            </form>
+            <div className={styles.seperator} />
+            <div className={styles.socialauth}>
+              <GoogleLoginButton />
+              <FacebookLoginButton />
+              <AppleLoginButton />
+            </div>
+          </div>
+          <div className={styles.nextstep}>
+            <div>
+              <input
+                type="submit"
+                form="UserPass Form"
+                value="Next Step"
+                className={styles.submit}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
