2a245f69ef7336847a770b8449b78bb2287f02c4
Janmejay-Joshi <publish.janmejay@gmail.com>
2022-02-22 21:04:33 +0530

    Added Chat Page

---
+++ pages/alumni/chat.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Navigation from "../../components/reusables/Navigation";
+import styles from "../../styles/PageStyles/Chat.module.scss";
+
+export default function Chat() {
+  return (
+    <div className={styles.container}>
+      <Navigation />
+      <main className={styles.main}>
+        <div className={styles.info_cards}>
+          <div className={styles.group_desc}></div>
+          <div className={styles.available_chats}></div>
+        </div>
+        <div className={styles.chat_container}>
+          <div className={styles.chat_wraper}>
+            <div className={styles.chat_box}>
+              {[0, 1, 2, 3, 4].map((data, index) => {
+                return data % 2 == 0 ? (
+                  <div className={styles.chat_recive}>
+                    <div className={styles.profile_pic}></div>
+                    <div className={styles.message_wraper}>
+                      <div className={styles.message}></div>
+                      <span className={styles.timestamp}></span>
+                    </div>
+                  </div>
+                ) : (
+                  <div className={styles.chat_sent}>
+                    <div className={styles.message_wraper}>
+                      <div className={styles.message}></div>
+                      <span className={styles.timestamp}></span>
+                    </div>
+                    <div className={styles.profile_pic}></div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className={styles.textbar_wraper}>
+              <div className={styles.textbar}>
+                <textarea className={styles.textbox}></textarea>
+                <div className={styles.send}></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
