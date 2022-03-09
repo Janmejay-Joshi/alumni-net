f87697cb99529cceb5a2092ca7951c2c9b2879d5
Codefreak69 <iamvikashkumar30@gmail.com>
2022-03-10 01:11:14 +0530

    Added My details page

--- components/reusables/Dashboard/Content/Content.tsx
+++ components/reusables/Dashboard/Content/Content.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import styles from "./Content.module.scss";
+import Profile from "./Profile/Profile";
 
 const Content = () => {
 
   
   return (
     <div className={styles.container}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil odit ad
-      cupiditate officiis sint, sit dignissimos eius maxime pariatur, sapiente
-      fuga magnam nemo unde soluta! Nam est sit quos ab nobis adipisci, nesciunt
-      totam at quasi aliquam natus ipsum ea assumenda pariatur doloremque
-      aspernatur, perspiciatis necessitatibus. Veniam fuga labore praesentium.
+    <Profile />
     </div>
   );
 };
--- components/reusables/Dashboard/Sidebar/Sidebar.tsx
+++ components/reusables/Dashboard/Sidebar/Sidebar.tsx
@@ -3,7 +3,11 @@ import styles from "./Sidebar.module.scss";
 import data from "./Data";
 import Link from "next/link";
 import { GiHamburgerMenu, GiRamProfile } from "react-icons/gi";
-import { IoIosArrowBack, IoIosArrowForward, IoMdNotificationsOutline } from "react-icons/io";
+import {
+  IoIosArrowBack,
+  IoIosArrowForward,
+  IoMdNotificationsOutline,
+} from "react-icons/io";
 import { GrContactInfo } from "react-icons/gr";
 import { AiOutlineProfile, AiOutlineTeam } from "react-icons/ai";
 import { BsFilePost } from "react-icons/bs";
@@ -13,65 +17,131 @@ import { CgDetailsMore } from "react-icons/cg";
 const Sidebar = () => {
   const [active, setActive] = useState(false);
 
-  const toggleActive = () =>{
+  const toggleActive = () => {
     setActive(!active);
-
-  }
+  };
   return (
     <>
-      <nav className={styles.sidebar} style={!active ? {width: "4rem"} : {} }>
+      <nav className={styles.sidebar} style={!active ? { width: "4rem" } : {}}>
         <header>
-        <div className={styles.header__text} style={!active ? {display: "none"} : {} }>
-          <span className={styles.header__text__title}>Alumni-Connect</span>
-        </div>
+          <div
+            className={styles.header__text}
+            style={!active ? { display: "none" } : {}}
+          >
+            <span className={styles.header__text__title}>Alumni-Connect</span>
+          </div>
 
-          <div className={styles.header__icon}  onClick={() => toggleActive()}>
-           { active ? <IoIosArrowBack /> : <IoIosArrowForward />}
+          <div className={styles.header__icon} onClick={() => toggleActive()}>
+            {active ? <IoIosArrowBack /> : <IoIosArrowForward />}
           </div>
         </header>
         <div className={styles.nav_menu}>
           <div className={styles.menu}>
             <ul className={styles.nav_links}>
               <li className={styles.nav_link}>
-                  <CgDetailsMore className={styles.icons} style={!active ? {position:  "relative", left :"-3.2rem"}  :{}}/>
+                <CgDetailsMore
+                  className={styles.icons}
+                  style={
+                    !active
+                      ? {
+                          position: "relative",
+                          left: "-3.2rem",
+                          fontSize: "1.8rem",
+                        }
+                      : {}
+                  }
+                />
                 <Link href="/">
                   <span className={styles.nav__text}>My Details</span>
                 </Link>
               </li>
               <li className={styles.nav_link}>
-                  <AiOutlineProfile className={styles.icons} style={!active ? {position:  "relative", left :"-3.2rem"}  :{}} />
+                <AiOutlineProfile
+                  className={styles.icons}
+                  style={
+                    !active
+                      ? {
+                          position: "relative",
+                          left: "-3.2rem",
+                          fontSize: "1.8rem",
+                        }
+                      : {}
+                  }
+                />
                 <Link href="/">
                   <span className={styles.nav__text}>Profile</span>
                 </Link>
               </li>
               <li className={styles.nav_link}>
-                  <BsFilePost className={styles.icons} style={!active ? {position:  "relative", left :"-3.2rem"}  :{}}/>
+                <BsFilePost
+                  className={styles.icons}
+                  style={
+                    !active
+                      ? {
+                          position: "relative",
+                          left: "-3.2rem",
+                          fontSize: "1.8rem",
+                        }
+                      : {}
+                  }
+                />
                 <Link href="/">
                   <span className={styles.nav__text}>Posts</span>
                 </Link>
               </li>
               <li className={styles.nav_link}>
-                  <IoMdNotificationsOutline className={styles.icons} style={!active ? {position:  "relative", left :"-3.2rem"}  :{}}/>
+                <IoMdNotificationsOutline
+                  className={styles.icons}
+                  style={
+                    !active
+                      ? {
+                          position: "relative",
+                          left: "-3.2rem",
+                          fontSize: "1.8rem",
+                        }
+                      : {}
+                  }
+                />
                 <Link href="/">
                   <span className={styles.nav__text}>Notices</span>
                 </Link>
               </li>
               <li className={styles.nav_link}>
-                  <AiOutlineTeam className={styles.icons} style={!active ? {position:  "relative", left :"-3.2rem"}  :{}}/>
+                <AiOutlineTeam
+                  className={styles.icons}
+                  style={
+                    !active
+                      ? {
+                          position: "relative",
+                          left: "-3.2rem",
+                          fontSize: "1.8rem",
+                        }
+                      : {}
+                  }
+                />
                 <Link href="/">
                   <span className={styles.nav__text}>Connections</span>
                 </Link>
               </li>
               <li className={styles.nav_link}>
-                  <FaDonate className={styles.icons} style={!active ? {position:  "relative", left :"-3.2rem"}  :{}}/>
+                <FaDonate
+                  className={styles.icons}
+                  style={
+                    !active
+                      ? {
+                          position: "relative",
+                          left: "-3.2rem",
+                          fontSize: "1.8rem",
+                        }
+                      : {}
+                  }
+                />
                 <Link href="/">
                   <span className={styles.nav__text}>Donations</span>
                 </Link>
               </li>
             </ul>
           </div>
-
-        
         </div>
       </nav>
     </>
