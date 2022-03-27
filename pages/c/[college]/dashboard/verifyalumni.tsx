26ac80b4ff0f5dd70860ef02bdf102b9a59a8aa1
Codefreak69 <iamvikashkumar30@gmail.com>
2022-03-27 19:12:32 +0530

    Verify Alumni fixed

--- pages/c/[college]/dashboard/verifyalumni.tsx
+++ pages/c/[college]/dashboard/verifyalumni.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Verifycard from '../../../../components/Cards/Verifycard';
 import DashboardLayout from '../../../../layout/DashboardLayout'
+import styles from "../../../../styles/PageStyles/College/Verify.module.scss";
+
 
 const VerifyAlumni = () => {
   return (
-    <div>Verifyalumni</div>
+    <>
+     <div className={styles.headers}>
+      <h1 className={styles.heading}>Alumni Management</h1>
+      <h3 className={styles.subheading}>Verify Heading</h3>
+      </div>
+      <div className={styles.main}>
+        <Verifycard />
+        <Verifycard />
+        <Verifycard />
+        <Verifycard />
+        <Verifycard />
+      </div>
+    </>
   )
 }
 
